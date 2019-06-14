97cecdae324b962ec48547b32af7715e2846961c
Jared Craig <jcraig1730@gmail.com>
2019-06-14 16:12:41 -0500

    Refactor _.extend and _.defaults
    Refactors the extend and deafults functions so they do not unnecesarily have to loop through the object being
    worked on.  Fix simply changes newObjs assignment from being an array of  all arguments given, to all argumnts
    given excluding the first.

--- src/underbar.js
+++ src/underbar.js
@@ -275,7 +275,7 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
-    var newObjs = [...arguments];
+    var newObjs = [...arguments].slice(1);
     _.each(newObjs, function(objToAdd){
       var keys = Object.keys(objToAdd);
       _.each(keys, function(objKey){
@@ -288,7 +288,7 @@
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
-    var newObjs = [...arguments];
+    var newObjs = [...arguments].slice(1);
     _.each(newObjs, function(objToAdd){
       var keys = Object.keys(objToAdd);
       _.each(keys, function(objKey){
